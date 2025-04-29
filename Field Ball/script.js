013f358dfbb6e7e96da0d7a88e723edd2e91d6ac
Ruhi K <152676409+ruhik8789@users.noreply.github.com>
2025-04-29 22:13:33 +0530

    Add files via upload

---
+++ Field Ball/script.js
@@ -0,0 +1,54 @@
+// field.onclick = function (event) {
+
+//     // window-relative field coordinates
+//     let fieldCoords = this.getBoundingClientRect();
+
+//     // the ball has position:absolute, the field: position:relative
+//     // so ball coordinates are relative to the field inner left-upper corner
+//     let ballCoords = {
+//         top: event.clientY - fieldCoords.top - field.clientTop - ball.clientHeight / 2,
+//         left: event.clientX - fieldCoords.left - field.clientLeft - ball.clientWidth / 2
+//     };
+
+//     // prevent crossing the top field boundary
+//     if (ballCoords.top < 0) ballCoords.top = 0;
+
+//     // prevent crossing the left field boundary
+//     if (ballCoords.left < 0) ballCoords.left = 0;
+
+
+//     // // prevent crossing the right field boundary
+//     if (ballCoords.left + ball.clientWidth > field.clientWidth) {
+//         ballCoords.left = field.clientWidth - ball.clientWidth;
+//     }
+
+//     // prevent crossing the bottom field boundary
+//     if (ballCoords.top + ball.clientHeight > field.clientHeight) {
+//         ballCoords.top = field.clientHeight - ball.clientHeight;
+//     }
+
+//     ball.style.left = ballCoords.left + 'px';
+//     ball.style.top = ballCoords.top + 'px';
+// }
+
+field.onclick = function(event) {
+    let fieldCoords = this.getBoundingClientRect();
+
+    let ballCoords = {
+        top: event.clientY - fieldCoords.top - field.clientTop - ball.clientHeight/2,
+        left: event.clientX - fieldCoords.left - field.clientLeft - ball.clientWidth/2
+    }
+
+    if(ballCoords.top < 0) ballCoords.top = 0;
+    if(ballCoords.left < 0) ballCoords.left = 0;
+
+    if(ballCoords.left + ball.clientWidth > field.clientWidth){
+        ballCoords.left = field.clientWidth - ball.clientWidth;
+    }
+    if(ballCoords.top + ball.clientHeight > field.clientHeight){
+        ballCoords.top = field.clientHeight - ball.clientHeight;
+    }
+
+    ball.style.left = ballCoords.left + 'px';
+    ball.style.top = ballCoords.top + 'px';
+}
